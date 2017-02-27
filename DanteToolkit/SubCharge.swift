ccb68a2b96331e84c8fed88d06769fd9bd8ccd19
danteg93 <dante@oomba.com>
2017-02-27 15:40:47 -0800

    Added settings and custom extensiosn for rounding

--- DanteToolkit/SubCharge.swift
+++ DanteToolkit/SubCharge.swift
@@ -9,5 +9,30 @@
 import Foundation
 
 class SubCharge {
+    var monetaryAmount: Double
+    var grandTotalAmount: Double
+    var percentOfTotal: Double
+    var nameOfCharge: String?
     
+    init(subAmount: Double, totalAmount: Double) {
+        self.monetaryAmount = subAmount
+        self.grandTotalAmount = totalAmount
+        self.percentOfTotal = (subAmount / totalAmount)
+    }
+    
+    public func getPercent(significantFigures sigFig: Int) -> Double? {
+        if sigFig < 0 && sigFig > 4 {
+            return nil
+        }
+        let exponentialPower = pow(10.0, Double(sigFig))
+        return Double(round(exponentialPower*percentOfTotal)/exponentialPower)
+    }
+    
+    public func getMonetaryAmount() -> Double {
+        return Double(round(10000*monetaryAmount)/10000)
+    }
+    
+    public func printableAmount() -> String {
+        return String(Double(round(100*monetaryAmount)/100))
+    }
 }
