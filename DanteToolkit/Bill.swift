9b534541c1e52e44ea3a9c753d27d0eb0b9025f5
danteg93 <dante@oomba.com>
2017-02-27 15:15:56 -0800

    Added base models for separation classes

---
+++ DanteToolkit/Bill.swift
@@ -0,0 +1,13 @@
+//
+//  Bill.swift
+//  DanteToolkit
+//
+//  Created by DANTE GARCIA RAMIREZ on 2/27/17.
+//  Copyright © 2017 Dante Studios. All rights reserved.
+//
+
+import Foundation
+
+class Bill {
+    
+}
---
+++ DanteToolkit/SubCharge.swift
@@ -0,0 +1,13 @@
+//
+//  SubCharge.swift
+//  DanteToolkit
+//
+//  Created by DANTE GARCIA RAMIREZ on 2/27/17.
+//  Copyright © 2017 Dante Studios. All rights reserved.
+//
+
+import Foundation
+
+class SubCharge {
+    
+}
